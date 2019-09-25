7b430e592c9de39911dfae167cf7047026f80146
jenry cacerez guzman <jenrybrown@hotmail.com>
2019-09-25 18:40:26 +0100

    primer Parcial

---
+++ Parcial1-AP/DAL/Contexto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Parcial1_AP.Entidades;
+
+namespace Parcial1_AP.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+        
+        public Contexto() : base("ConStr")
+        {
+            
+        }
+
+
+
+    }
+
+}
